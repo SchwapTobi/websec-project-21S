4984b34ff8612a5d6d46d091da7a32e56bc08e01
SchwapTobi <tobias.schwap@gmail.com>
2021-07-10 13:26:27 +0200

    added test cases for both endpoints

--- tests/secure.test.ts
+++ tests/secure.test.ts
@@ -4,15 +4,44 @@ const supertest = require("supertest");
 const BASEURL = 'http://localhost:8080/secure';
 
 describe('security tests', function () {
-    // hello world test
-    it('should return something', async () => {
+
+    it('should return unauthorized', async () => {
         await supertest(BASEURL)
-            .get('/test')
+            .get('/userProfile')
+            .expect(401)
+            .then((response: any) => {
+                //check response
+                expect(_.isObject(response.body)).toBeTruthy();
+                expect(_.get(response.body, 'status')).toBe('invalid cookie')
+            });
+    });
+
+    it('should return wrong password', async () => {
+        await supertest(BASEURL)
+            .get('/userProfile')
+            .set('cookie', 'eyJpZCI6IjEzMzciLCJwYXNzd29yZCI6Indyb25ncGFzc3dvcmQifQo=')
+            .expect(401)
+            .then((response: any) => {
+                //check response
+                expect(_.isObject(response.body)).toBeTruthy();
+                expect(_.get(response.body, 'status')).toBe('invalid password')
+            });
+    });
+
+    it('should return user data', async () => {
+        await supertest(BASEURL)
+            .get('/userProfile')
+            .set('cookie', 'eyJpZCI6IjEzMzciLCJwYXNzd29yZCI6ImhlbGxvIn0=')
             .expect(200)
             .then((response: any) => {
                 //check response
                 expect(_.isObject(response.body)).toBeTruthy();
-            })
+                expect(_.get(response, 'body.id', null)).toBe('1337')
+                expect(_.get(response, 'body.password', null)).toBe('hello')
+                expect(_.get(response, 'body.name', null)).toBe('Mallory')
+                expect(_.get(response, 'body.points', null)).toBe(0)
+                expect(_.get(response, 'body.role', null)).toBe('user')
+            });
     });
 
 });
